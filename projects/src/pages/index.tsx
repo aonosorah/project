c2e2f9876366ac46b2eef361deec7388d0e4f13e
aonosorah <102835599+aonosorah@users.noreply.github.com>
2024-08-24 14:15:44 -0300

    error handling added

--- projects/src/pages/index.tsx
+++ projects/src/pages/index.tsx
@@ -10,14 +10,23 @@ export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [sortColumn, setSortColumn] = useState<string | null>(null);
   const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+  const [error, setError] = useState<string | null>(null);
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
 
   const handleFetchUsers = async () => {
     try {
       const data = await fetchUsers();
       setUsers(data);
     } catch (error) {
-      console.error('Erro ao buscar usuários:', error);
+      console.error('Unexpected error locating user:', error);
+      setError('An error occured while searching the user.Try again later');
+      setIsErrorModalOpen(true);
     }
+  };
+
+  const closeErrorModal = () => {
+    setIsErrorModalOpen(false);
+    setError(null);
   };
 
   useEffect(() => {
@@ -37,9 +46,17 @@ export default function Home() {
     return path.split('.').reduce((value, key) => value[key], obj);
   };
 
-  const filteredUsers = users.filter((user) =>
-    user.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredUsers = users.filter((user) => {
+    const lowercasedTerm = searchTerm.toLowerCase();
+    return (
+      user.name.toLowerCase().includes(lowercasedTerm) ||
+      user.username.toLowerCase().includes(lowercasedTerm) ||
+      user.email.toLowerCase().includes(lowercasedTerm) ||
+      user.phone.toLowerCase().includes(lowercasedTerm) ||
+      user.address.city.toLowerCase().includes(lowercasedTerm) ||
+      user.company.name.toLowerCase().includes(lowercasedTerm)
+    );
+  });
 
   const sortedUsers: IUser[] = [...filteredUsers].sort((a, b) => {
     if (sortColumn) {
@@ -67,7 +84,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Users</h1>
+      <h1 className="text-2xl font-bold mb-4 text-white">Users</h1>
       <div className="mb-4">
         <input
           type="text"
@@ -128,16 +145,24 @@ export default function Home() {
           </tr>
         </thead>
         <tbody>
-          {sortedUsers.map((user) => (
-            <tr key={user.id} onClick={() => handleUserClick(user)}>
-              <td className="border p-2 text-white">{user.name}</td>
-              <td className="border p-2 text-white">{user.username}</td>
-              <td className="border p-2 text-white">{user.email}</td>
-              <td className="border p-2 text-white">{user.phone}</td>
-              <td className="border p-2 text-white">{user.address.city}</td>
-              <td className="border p-2 text-white">{user.company.name}</td>
+          {sortedUsers.length ? (
+            sortedUsers.map((user) => (
+              <tr key={user.id} onClick={() => handleUserClick(user)}>
+                <td className="border p-2 text-white">{user.name}</td>
+                <td className="border p-2 text-white">{user.username}</td>
+                <td className="border p-2 text-white">{user.email}</td>
+                <td className="border p-2 text-white">{user.phone}</td>
+                <td className="border p-2 text-white">{user.address.city}</td>
+                <td className="border p-2 text-white">{user.company.name}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={6} className="border p-2 text-center text-white">
+                This user could not be located
+              </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
 
@@ -146,6 +171,22 @@ export default function Home() {
         isOpen={isModalOpen}
         onClose={closeModal}
       />
+      {isErrorModalOpen && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white rounded-lg shadow-lg p-6 w-96">
+            <h2 className="text-xl font-semibold mb-4">Error</h2>
+            <p>{error}</p>
+            <div className="flex justify-end mt-4">
+              <button
+                onClick={closeErrorModal}
+                className="bg-red-500 text-white p-2 rounded"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
